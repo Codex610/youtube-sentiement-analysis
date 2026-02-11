e01ba39720b8d2ac37fead22cecde98f8a4e1e3c
Rajneesh chauhan <rajnishchauhan912556@gmail.com>
2026-02-11 19:32:52 +0530

    updated

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # reddit-sentiement-analysis
+
+conda create -n youtube python=3.11 -y
+
+conda activate youtube
+
+pip install -r requirements.txt
+
+## DVC
+git init
+
+dvc init
+
+dvc repro
+
+dvc dag
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='youtube',
+    version='0.0.0',
+    author='codex',
+    author_email='c41398782@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
